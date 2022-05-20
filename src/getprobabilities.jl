cd6380404fb5facdcb734d362d831fb4d919d3e8
ngiann <ngiann@gmail.com>
2022-05-20 12:27:49 +0200

    return probabilities in same shape as argument out

--- src/getprobabilities.jl
+++ src/getprobabilities.jl
@@ -4,6 +4,8 @@ function getprobabilities(out)
 
     nfolds = size(Q, 2)
 
-    vec(mean(reduce(hcat, [exp.(Q[:,i] .- logsumexp(Q[:,i])) for i in 1:nfolds]), dims=2))
+    aux = vec(mean(reduce(hcat, [exp.(Q[:,i] .- logsumexp(Q[:,i])) for i in 1:nfolds]), dims=2))
 
+    reshape(aux, size(out))
+    
 end
