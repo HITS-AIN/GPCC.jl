f0fb94b2df082a979d669dffb060cb04cdff87a8
ngiann <ngiann@gmail.com>
2022-08-10 09:41:06 +0200

    function for posterior probabilty  takes prior into account

--- src/getprobabilities.jl
+++ src/getprobabilities.jl
@@ -1,5 +1,21 @@
 function getprobabilities(out)
 
+    flatpriorvalues = ones(size(out))
+
+    getprobabilities(out, flatpriorvalues)
+
+end
+
+function getprobabilities(out, priorpdfvalues)
+
+    # add log prior to form log joint likelihood
+
+    for i in eachindex(out)
+        
+        out[i] .+= log(priorpdfvalues[i])
+
+    end
+
     Q = reduce(hcat, out)'
 
     nfolds = size(Q, 2)
