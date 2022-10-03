1ca26859c86d0e40dc6de73ae2748bf9f9f63511
ngiann <ngiann@gmail.com>
2022-10-03 20:22:32 +0200

    simplify calculation of posterior after discarding performcv

--- src/getprobabilities.jl
+++ src/getprobabilities.jl
@@ -1,27 +1,23 @@
-function getprobabilities(out)
+function getprobabilities(loglikel)
 
-    flatpriorvalues = ones(size(out))
+    flatpriorvalues = ones(size(loglikel))
 
-    getprobabilities(out, flatpriorvalues)
+    getprobabilities(loglikel, flatpriorvalues)
 
 end
 
-function getprobabilities(out, priorpdfvalues)
+function getprobabilities(loglikel, logpriorpdfvalues)
 
     # add log prior to form log joint likelihood
 
-    for i in eachindex(out)
+    for i in eachindex(loglikel)
         
-        out[i] .+= log(priorpdfvalues[i])
+        loglikel[i] += logpriorpdfvalues[i]
 
     end
 
-    Q = reduce(hcat, out)'
+    posterior = exp.(loglikel .- logsumexp(loglikel))
 
-    nfolds = size(Q, 2)
-
-    aux = vec(mean(reduce(hcat, [exp.(Q[:,i] .- logsumexp(Q[:,i])) for i in 1:nfolds]), dims=2))
-
-    reshape(aux, size(out))
+    return posterior
     
 end
