156abcc1f2ad6da66c0bd71718971c47d56168ad
Nikos Gianniotis <ngiann@gmail.com>
2024-07-08 13:44:25 +0200

    avoid BlockMatrices.jl, make faster

--- src/delayedCovariance.jl
+++ src/delayedCovariance.jl
@@ -18,20 +18,42 @@ function delayedCovariance(kernel, scale, delays, ρ, x, y)
 
     # it is convenient to use a block array to calculate the covariance matrix
 
-    B = PseudoBlockArray(zeros(eltype(scale), sum(Nx)*L, sum(Ny)*L), Nx, Ny)
+    # B = PseudoBlockArray(zeros(eltype(scale), sum(Nx)*L, sum(Ny)*L), Nx, Ny)
 
-    for i in 1:L
+    # for i in 1:L
 
-        for j in 1:L
+    #     for j in 1:L
 
-            B[Block(i,j)] = [scale[i] * scale[j] * kernel(x₁-delays[i],x₂-delays[j]; ρ=ρ)  for x₁ in x[i], x₂ in y[j]]
+    #         B[Block(i,j)] = [scale[i] * scale[j] * kernel(x₁-delays[i],x₂-delays[j]; ρ=ρ)  for x₁ in x[i], x₂ in y[j]]
 
-        end
+    #         # @show (i,j) size(B[Block(i,j)])
+    #     end
+
+    # end
+
+    # return Matrix(B)
+
+    # Equivalent to above but faster
+    
+    B = zeros(sum(Nx), sum(Ny))
+
+    function aux(x, y, dx, dy)
+        local D² = pairwise(SqEuclidean(), x .- dx, y .- dy)
+        exp.(-0.5*D²/(2ρ))
+    end
+
+    for i in 1:L, j in 1:L
+
+        st = (1+sum(Nx[1:i-1])):sum(Nx[1:i])
+
+        en = (1+sum(Ny[1:j-1])):sum(Ny[1:j])
+        
+        B[st,en] = scale[i] * scale[j] * aux(x[i], y[j], delays[i], delays[j])
 
     end
 
-    return Matrix(B)
-
+    return B
+    
 end
 
 
