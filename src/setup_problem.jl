215f65621a5f56df701e526b8f6d0e36b76243a9
Nikos Gianniotis <ngiann@gmail.com>
2024-07-18 21:29:32 +0200

    update convenience function

--- src/setup_problem.jl
+++ src/setup_problem.jl
@@ -2,10 +2,24 @@ function setup_problem(tobs, yobs, σobs; kernel = GPCC.matern52, iterations = 1
 
     ρfixed = infercommonlengthscale(tobs, yobs, σobs; kernel = kernel, iterations = iterations, seed = seed, numberofrestarts = numberofrestarts, initialrandom = initialrandom, ρmin = ρmin, ρmax = ρmax, verbose = false)[3][3]
 
-    function helper(delay...) 
+    function helper(delay::T...) where T<:Real
   
-        gpcc(tobs, yobs, σobs; kernel = kernel, delays = vcat(0, delay...), iterations = iterations, rhomax = ρmax, ρfixed = ρfixed)
+        gpcc(tobs, yobs, σobs; kernel = kernel, delays = vcat(0, delay...), iterations = iterations, rhomax = ρmax, ρfixed = ρfixed)[1]
     
+    end
+
+    function helper(delay::Vector{T}...) where T<:Real
+  
+        @showprogress tmap(x->helper(x...), Iterators.product(delay...));
+
+    end
+
+    L = length(tobs)
+
+    function helper(delay::Vector{T}) where T<:Real
+  
+        helper(ntuple(i -> delay, L-1)...)
+
     end
 
 end
